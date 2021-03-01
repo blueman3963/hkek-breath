c12ecd55188acb319c00f418048aa881fe72f939
Yifu <>
2021-03-01 10:00:20 -0500

    add blending mode

--- src/components/module1/index.js
+++ src/components/module1/index.js
@@ -1,6 +1,8 @@
 import { useState, useRef } from 'react'
 import Main from './main'
 
+import bg from './asset/bg.jpg'
+
 import styles from './module1.module.scss'
 
 export default function Module1 () {
@@ -17,10 +19,12 @@ export default function Module1 () {
     return <div>
         {
             !init
-            ? <div className={styles.intro}>
-                <input onChange={e => setName(e.target.value)}ref={input} className={styles.input} placeholder='Name' maxLength='20'/>
-                <div onClick={() => process()} className={styles.submit}>GO</div>
+            ? <><div className={styles.intro}>
+                <input onChange={e => setName(e.target.value)}ref={input} className={styles.input} placeholder='Name' maxLength='12'/>
+                <div onClick={() => process()} className={styles.submit}>&#x2192;</div>
             </div>
+            <img src={bg} className={styles.bg} />
+            </>
             : <Main name={name}/>
         }
     </div>
